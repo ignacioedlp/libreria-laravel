00406fc8b9b348c6a1fc79bb5a84d7915c641a6f
Ignacio Cafiero Torrubia <ignaciocafiero12@gmail.com>
2022-01-13 21:15:44 -0300

    first commit

---
+++ resources/views/libro/form.blade.php
@@ -0,0 +1,20 @@
+<div class="box box-info padding-1">
+    <div class="box-body">
+
+        <div class="form-group">
+            {{ Form::label('category_id') }}
+            {{ Form::select('category_id',$categorias, $libro->category_id, ['class' => 'form-control' . ($errors->has('category_id') ? ' is-invalid' : ''), 'placeholder' => 'Category Id']) }}
+            {!! $errors->first('category_id', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+
+        <div class="form-group">
+            {{ Form::label('nombre') }}
+            {{ Form::text('nombre', $libro->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
+            {!! $errors->first('nombre', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+</div>
